1dac11c75e89514fc64e6cffd237b87f4aaf85ba
Malikabrar01 <malikabrar4477@gmail.com>
2025-08-27 18:56:51 +0000

    Date And Time

---
+++ .devcontainer/basics_01/dateAndTime.js
@@ -0,0 +1,7 @@
+let myDate=new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+
+//let newTime=Date.now()
+console.log(Math.floor(Date.now()/1000))
